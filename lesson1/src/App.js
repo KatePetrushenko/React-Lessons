e31e534e09e53f6f53e869bd2498c251528f091d
unknown <kateryna.kolisnyk@p2h.com>
2020-11-10 22:15:06 +0200

    add drop updates

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dropdown from "./Dropdown";
 import Сounter from './Сounter';
+import './App.css';
+
+const dropOptions = [
+  { option: "option1" },
+  { option: "option2" },
+  { option: "option3" }
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {count: 0};
   }
+
   handleClick = () => {
-    this.setState({count : this.state.count + 1});
+    this.setState( curState => {
+      return {
+        count : curState.count + 1
+      }
+    })
   }
 
   render(){
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <button onClick={this.handleClick}>Test Button</button>
-      
-      <Сounter count={this.state.count} />
-    </div>
+      <div className="App">
+        <button onClick={this.handleClick}>Test Button</button>
+        <Сounter count={this.state.count} />
+        <Dropdown 
+          holderClassName = 'dropdown-holder'
+          className="dropdown"
+          openerClassName="drop-opener"
+          options={dropOptions}
+        />
+      </div>
     )
   }
 }
